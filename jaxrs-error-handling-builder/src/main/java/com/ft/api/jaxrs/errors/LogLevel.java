d0c59bde096d8246735bb44a564198426f49db14
Simon.Gibbs <simon.gibbs@ft.com>
2014-11-13 09:35:38 +0000

    Client error log level support

--- jaxrs-error-handling-builder/src/main/java/com/ft/api/jaxrs/errors/LogLevel.java
+++ jaxrs-error-handling-builder/src/main/java/com/ft/api/jaxrs/errors/LogLevel.java
@@ -13,11 +13,20 @@ public enum LogLevel {
         public void logTo(Logger logger, String message, Throwable cause) {
             logger.debug(message, cause);
         }
+
+        @Override
+        public void logTo(Logger logger, String message, Object... args) {
+            logger.debug(message, args);
+        }
     },
     INFO {
         @Override
         public void logTo(Logger logger, String message, Throwable cause) {
             logger.info(message, cause);
+        }
+        @Override
+        public void logTo(Logger logger, String message, Object... args) {
+            logger.info(message, args);
         }
     },
     WARN {
@@ -25,18 +34,27 @@ public enum LogLevel {
         public void logTo(Logger logger, String message, Throwable cause) {
             logger.warn(message, cause);
         }
+        @Override
+        public void logTo(Logger logger, String message, Object... args) {
+            logger.warn(message, args);
+        }
     },
     ERROR {
         @Override
         public void logTo(Logger logger, String message, Throwable cause) {
             logger.error(message, cause);
         }
+        @Override
+        public void logTo(Logger logger, String message, Object... args) {
+            logger.error(message, args);
+        }
     };
 
     public abstract void logTo(Logger logger, String message, Throwable cause);
+    public abstract void logTo(Logger logger, String message, Object... args);
 
     public void logTo(Logger logger, String message) {
-        this.logTo(logger, message, null);
+        this.logTo(logger, message, (Throwable) null);
     }
 
 }
